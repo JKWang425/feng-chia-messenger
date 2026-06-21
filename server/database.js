15eb49edb2ae909a8d8674f0e89dba626e00eaac
JKWang425 <wjulie0425@gmail.com>
2026-06-22 04:26:28 +0800

    feat: initialize Azure SQL connection pool and database schema

--- server/database.js
+++ server/database.js
@@ -6,6 +6,7 @@ const config = {
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
+    connectionTimeout: 30000,
     options: {
         encrypt: true,
         trustServerCertificate: false
